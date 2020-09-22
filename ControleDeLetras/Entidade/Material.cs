e2d008bf42aa1dc58a5c6c6722dba38a116ed728
Paulo Mendes <motoredi@yahoo.com.br>
2020-09-22 01:43:21 -0300

    Acrescentando tela Materiais

---
+++ ControleDeLetras/Entidade/Material.cs
@@ -0,0 +1,14 @@
+﻿namespace ControleDeLetras.Entidade
+{
+    public class Material
+    {
+        public Material()
+        {
+            Id = int.MinValue;
+        }
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ ControleDeLetras/Forms/FrmMateriais.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ControleDeLetras.Forms
+{
+    partial class FrmMateriais
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtAcresc = new System.Windows.Forms.NumericUpDown();
+            this.txtQtde = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnAcresc = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.dgvMateriais = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAcresc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQtde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMateriais)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtAcresc);
+            this.panel1.Controls.Add(this.txtQtde);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtDescricao);
+            this.panel1.Controls.Add(this.btnRemover);
+            this.panel1.Controls.Add(this.btnAlterar);
+            this.panel1.Controls.Add(this.btnAcresc);
+            this.panel1.Controls.Add(this.btnAdicionar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 82);
+            this.panel1.TabIndex = 8;
+            // 
+            // txtAcresc
+            // 
+            this.txtAcresc.Location = new System.Drawing.Point(275, 24);
+            this.txtAcresc.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.txtAcresc.Minimum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            -2147483648});
+            this.txtAcresc.Name = "txtAcresc";
+            this.txtAcresc.Size = new System.Drawing.Size(43, 20);
+            this.txtAcresc.TabIndex = 7;
+            this.txtAcresc.ValueChanged += new System.EventHandler(this.txtAcresc_ValueChanged);
+            // 
+            // txtQtde
+            // 
+            this.txtQtde.Location = new System.Drawing.Point(211, 24);
+            this.txtQtde.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.txtQtde.Name = "txtQtde";
+            this.txtQtde.Size = new System.Drawing.Size(43, 20);
+            this.txtQtde.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Qtde:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Descrição:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDescricao.Location = new System.Drawing.Point(8, 25);
+            this.txtDescricao.MaxLength = 30;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(197, 20);
+            this.txtDescricao.TabIndex = 0;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(143, 51);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(62, 23);
+            this.btnRemover.TabIndex = 6;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(76, 51);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(62, 23);
+            this.btnAlterar.TabIndex = 5;
+            this.btnAlterar.Text = "Modificar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnAcresc
+            // 
+            this.btnAcresc.Enabled = false;
+            this.btnAcresc.Location = new System.Drawing.Point(262, 50);
+            this.btnAcresc.Name = "btnAcresc";
+            this.btnAcresc.Size = new System.Drawing.Size(72, 23);
+            this.btnAcresc.TabIndex = 3;
+            this.btnAcresc.Text = "Acrescentar";
+            this.btnAcresc.UseVisualStyleBackColor = true;
+            this.btnAcresc.Click += new System.EventHandler(this.btnAcresc_Click);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(8, 51);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(62, 23);
+            this.btnAdicionar.TabIndex = 4;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // dgvMateriais
+            // 
+            this.dgvMateriais.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMateriais.Location = new System.Drawing.Point(12, 103);
+            this.dgvMateriais.Name = "dgvMateriais";
+            this.dgvMateriais.ReadOnly = true;
+            this.dgvMateriais.Size = new System.Drawing.Size(344, 342);
+            this.dgvMateriais.TabIndex = 7;
+            this.dgvMateriais.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMateriais_CellClick);
+            // 
+            // FrmMateriais
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 450);
+            this.Controls.Add(this.dgvMateriais);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmMateriais";
+            this.Text = "Materiais";
+            this.Load += new System.EventHandler(this.FrmLetras_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAcresc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQtde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMateriais)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.DataGridView dgvMateriais;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAcresc;
+        private System.Windows.Forms.NumericUpDown txtAcresc;
+        private System.Windows.Forms.NumericUpDown txtQtde;
+    }
+}
