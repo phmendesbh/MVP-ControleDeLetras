faa11629e2f92e82dbdeac6fb6762045ed85b58e
Paulo Mendes <motoredi@yahoo.com.br>
2020-09-29 17:36:40 -0300

    Criação da Tela Tipo Material

--- ControleDeLetras/Forms/FrmTipo_Material.Designer.cs
+++ ControleDeLetras/Forms/FrmTipo_Material.Designer.cs
@@ -28,21 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvMateriais = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMateriais)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dgvMateriais
+            // 
+            this.dgvMateriais.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMateriais.Location = new System.Drawing.Point(12, 103);
+            this.dgvMateriais.Name = "dgvMateriais";
+            this.dgvMateriais.ReadOnly = true;
+            this.dgvMateriais.Size = new System.Drawing.Size(344, 342);
+            this.dgvMateriais.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtDescricao);
+            this.panel1.Controls.Add(this.btnRemover);
+            this.panel1.Controls.Add(this.btnAlterar);
+            this.panel1.Controls.Add(this.btnAdicionar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 82);
+            this.panel1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Descrição:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(8, 25);
+            this.txtDescricao.MaxLength = 30;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(197, 20);
+            this.txtDescricao.TabIndex = 0;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(143, 51);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(62, 23);
+            this.btnRemover.TabIndex = 6;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(76, 51);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(62, 23);
+            this.btnAlterar.TabIndex = 5;
+            this.btnAlterar.Text = "Modificar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(8, 51);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(62, 23);
+            this.btnAdicionar.TabIndex = 4;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
             // FrmTipo_Material
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(370, 450);
+            this.Controls.Add(this.dgvMateriais);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FrmTipo_Material";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tipo de Materiais";
+            this.Load += new System.EventHandler(this.FrmTipo_Material_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMateriais)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvMateriais;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnAdicionar;
     }
 }
--- ControleDeLetras/Forms/FrmTipo_Material.cs
+++ ControleDeLetras/Forms/FrmTipo_Material.cs
@@ -1,4 +1,5 @@
-﻿using ControleDeLetras.Repositorio;
+﻿using ControleDeLetras.Entidade;
+using ControleDeLetras.Repositorio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,12 +15,73 @@ namespace ControleDeLetras.Forms
     public partial class FrmTipo_Material : Form
     {
         readonly Tipo_MaterialRepositorio Tipo_MaterialRepositorio = new Tipo_MaterialRepositorio();
+        private Tipo_Material tipo_materialSelecionado = new Tipo_Material();
 
         public FrmTipo_Material()
         {
             InitializeComponent();
             Tipo_MaterialRepositorio.VerificaBanco();
 
+        }
+
+        private void FrmTipo_Material_Load(object sender, EventArgs e)
+        {
+            AtualizaTela();
+        }
+
+        private void AtualizaTela()
+        {
+            txtDescricao.Text = "";
+
+            dgvMateriais.DataSource = Tipo_MaterialRepositorio.Obter();
+            dgvMateriais.Columns[0].Visible = false;
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDescricao.Text)) return;
+
+            var retorno = MessageBox.Show($"Confirma inclusão do Tipo de Material '{txtDescricao.Text}' ?", "Adicionar", MessageBoxButtons.YesNo);
+
+            if (retorno == DialogResult.Yes)
+            {
+                Tipo_MaterialRepositorio.Inserir(new Tipo_Material()
+                {
+                    Descricao = txtDescricao.Text,
+                });
+                AtualizaTela();
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (tipo_materialSelecionado.Id == int.MinValue || string.IsNullOrWhiteSpace(txtDescricao.Text)) return;
+
+            var retorno = MessageBox.Show($"Confirma alteração do Tipo de Material {tipo_materialSelecionado.Descricao}?", "Alterar", MessageBoxButtons.YesNo);
+
+            if (retorno == DialogResult.Yes)
+            {
+                Tipo_MaterialRepositorio.Alterar(new Tipo_Material()
+                {
+                    Id = tipo_materialSelecionado.Id,
+                    Descricao = txtDescricao.Text
+                });
+                AtualizaTela();
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (tipo_materialSelecionado.Id == int.MinValue) return;
+
+            var retorno = MessageBox.Show($"Confirma remover o material {txtDescricao.Text} ?", "Apagar", MessageBoxButtons.YesNo);
+
+            if (retorno == DialogResult.Yes)
+            {
+                Tipo_MaterialRepositorio.Remover(tipo_materialSelecionado.Id);
+
+                AtualizaTela();
+            }
         }
     }
 }
