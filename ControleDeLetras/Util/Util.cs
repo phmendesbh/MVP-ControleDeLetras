49fb109f1d93ac00a07d5a97691f0f75920d1d9f
Paulo Mendes <motoredi@yahoo.com.br>
2020-09-29 00:01:08 -0300

    Classe Utilitaria

---
+++ ControleDeLetras/Util/Util.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace ControleDeLetras.Util
+{
+    public class Enumeradores
+    {
+        public enum Operacao
+        {
+            Negativo = -1,
+            Zero = 0,
+            Positivo = 1
+        }
+    }
+
+    public class Utils
+    {
+        public IDictionary<string, int> CalculaQtdeLetras(List<string> lstPalavras)
+        {
+            IDictionary<string, int> letrasQtde = new Dictionary<string, int>();
+
+            lstPalavras.ForEach(palavra =>
+            {
+                var letras = palavra.ToUpper().ToCharArray();
+                foreach (var letra in letras)
+                {
+                    if (letrasQtde.ContainsKey(letra.ToString()))
+                    {
+                        letrasQtde[letra.ToString()] = letrasQtde[letra.ToString()] + 1;
+                    }
+                    else
+                    {
+                        letrasQtde.Add(letra.ToString(), 1);
+                    }
+                }
+            });
+
+            return new SortedDictionary<string, int>(letrasQtde);
+        }
+    }
+}
