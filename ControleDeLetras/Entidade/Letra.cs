8c56b1469bc1cf304f1019a2e8028fdbec4752f6
Paulo Mendes <motoredi@yahoo.com.br>
2020-10-04 16:13:22 -0300

    Retirando Classe não utilizada

--- ControleDeLetras/Entidade/Letra.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ControleDeLetras.Entidade
-{
-    public class Letra
-    {
-        public int Id { get; set; }
-        public string Descricao { get; set; }
-        public int Quantidade { get; set; }
-    }
-}
